6693932f5385da1471fe22883c8ae02a96b069df
Eric Miller <eric.miller@moodys.com>
2020-03-13 17:01:37 -0500

    refactored gulp to v4 and replaced LESS with SASS Compiler

--- gulpfile.js
+++ gulpfile.js
@@ -2,91 +2,47 @@
 
 "use strict";
 
-const FOLDER_SRC = "src"
-    , FOLDER_DIST = "dist"
-    , FOLDER_JS = "js"
-    , FOLDER_CSS = "css"
-    , FOLDER_SASS = "scss"
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    minifyCSS = require("gulp-minify-css"),
+    notify = require("gulp-notify"),
+    clean = require("gulp-clean"),
+    rename = require("gulp-rename"),
+    uglify = require("gulp-uglify");
 
-    , FOLDER_SRC_SCSS = FOLDER_SRC + "/" + FOLDER_SASS
-    , FOLDER_SRC_JS = FOLDER_SRC + "/" + FOLDER_JS
-    , FOLDER_DIST_CSS = FOLDER_DIST + "/" + FOLDER_CSS
-    , FOLDER_DIST_JS = FOLDER_DIST + "/" + FOLDER_JS
-    ;
-
-
-//var gulp = require("gulp"),
-//    sass = require("gulp-sass"),
-//    minifyCSS = require("gulp-minify-css"),
-//    notify = require("gulp-notify"),
-//    clean = require("gulp-clean"),
-//    rename = require("gulp-rename"),
-//    uglify = require("gulp-uglify");
-
-//gulp.task("sass", function () {
-//    return gulp.src(FOLDER_SRC_SCSS + "/bootstrap-dialog.scss")
-//        .pipe(sass())
-//        .pipe(gulp.dest(FOLDER_DIST_CSS))
-//});
-
-//gulp.task("minifycss", function () {
-//    return gulp.src(FOLDER_DIST_CSS + "/**/*.css")
-//        .pipe(minifyCSS())
-//        .pipe(gulp.dest(FOLDER_DIST_CSS))
-//});
-
-//gulp.task("lint", function () {
-//    gulp.src([FOLDER_SRC_JS + "/bootstrap-dialog.js"])
-//        .pipe(eslint())
-//        .pipe(eslint.format());
-//});
-
-//gulp.task("dist", ["clean", "sass"], function () {
-//    gulp.src([FOLDER_SRC_JS + "/bootstrap-dialog.js"])
-//        .pipe(gulp.dest(FOLDER_DIST_JS))
-//        .pipe(rename("bootstrap-dialog.min.js"))
-//        .pipe(uglify())
-//        .pipe(gulp.dest(FOLDER_DIST_JS))
-//        .pipe(notify({
-//            message: "Build task completed."
-//        }));
-//});
-
-//gulp.task("clean", function () {
-//    return gulp.src([FOLDER_DIST + "/"], {
-//        read: false
-//    })
-//        .pipe(clean());
-//});
-
-//gulp.task("default", ["clean"], function () {
-//    //gulp.start("dist");
-//});
-
-const { series, parallel, src, dest } = require('gulp');
-const clean = require("gulp-clean");
-const sass = require("gulp-sass");
-const minifyCSS = require("gulp-minify-css");
-
-function reset(cb) {
-    src([FOLDER_DIST + "/**/"], { read: false }).pipe(clean());
-    cb();
-}
-
-function css(cb) {
-    src(FOLDER_SRC_SCSS + "/bootstrap-dialog.scss")
+gulp.task("sass", function (cb) {
+    gulp.src("src/scss/bootstrap-dialog.scss")
         .pipe(sass())
-        .pipe(dest(FOLDER_DIST_CSS));
-
-    src(FOLDER_DIST_CSS + "/**/*.css")
+        .pipe(gulp.dest("dist/css"))
+        .pipe(gulp.dest("src/css"))
+        .pipe(rename("bootstrap-dialog.min.css"))
         .pipe(minifyCSS())
-        .pipe(dest(FOLDER_DIST_CSS));
+        .pipe(gulp.dest("dist/css"))
+        .pipe(notify({
+            message: "SASS task completed."
+        }));
 
     cb();
-}
+});
 
-function js(cb) {
+gulp.task("js", function (cb) {
+    gulp.src(["src/js/bootstrap-dialog.js"])
+        .pipe(gulp.dest("dist/js"))
+        .pipe(rename("bootstrap-dialog.min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("dist/js"))
+        .pipe(notify({
+            message: "JS task completed."
+        }));
+
     cb();
-}
+});
 
-exports.default = series(reset, parallel(css, js));
+gulp.task("clean", function (cb) {
+    gulp.src(["dist/"], { read: false, allowEmpty: true })
+        .pipe(clean())
+
+    cb();
+});
+
+gulp.task("default", gulp.series("clean", gulp.parallel("js", "sass")));
